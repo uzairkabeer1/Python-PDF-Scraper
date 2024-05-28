ee3f5d5eb3cc03e8a9189a00005349d7381ed308
Muhammad Uzair kabeer <muhammaduzairkabeer@gmail.com>
2024-05-28 13:47:07 +0500

    using PyMuPDF

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Python-PDF-Scraper
+
+## Previous version
 In this file, I used pdfQuery library and with the help of pdf->xml.
 I get the specific pdf data.
+
+## Newer version
+This version used PyMuPDF and fitz library to able to extract the hightlighted text from pdf. it will require no xml conversion and is alot faster and fairly more accurate.
+Before running it, run the command:
+`pip install fitz PyMuPDF`
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import pdfquery
+import fitz  
 
-pdf = pdfquery.PDFQuery('BUSTA_PAGA - 2.pdf')
-pdf.load()
-pdf.tree.write('pdfXML.txt', pretty_print = True)
+def extract_highlighted_text_from_pdf(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    highlighted_text = []
 
-Name = pdf.pq('LTTextLineHorizontal:overlaps_bbox("25.509, 729.284, 102.06, 737.284")').text()
-Nam = pdf.pq('LTTextLineHorizontal:overlaps_bbox("25.509, 740.621, 97.031, 748.621")').text()
+    for page_num in range(len(pdf_document)):
+        page = pdf_document[page_num]
+        annotations = page.annots()
+        if annotations:
+            for annot in annotations:
+                if annot.type[0] == 8:  
+                    rect = annot.rect  
+                    text = page.get_text("text", clip=rect)  
+                    highlighted_text.append(text.strip())
 
-N = pdf.pq('LTTextLineHorizontal:overlaps_bbox("25.509, 706.61, 188.558, 714.61")').text()
+    pdf_document.close()
+    return highlighted_text
 
-A = pdf.pq('LTTextLineHorizontal:overlaps_bbox("425.166, 668.04, 570.398, 676.04")').text()
-B = pdf.pq('LTTextLineHorizontal:overlaps_bbox("538.588, 479.073, 569.716, 515.873")').text()
-
-print(Name,'\n',Nam,'\n',N,'\n',A,'\n',"Competenze:",B)
+pdf_path = "BUSTA_PAGA - 2.pdf"
+highlighted_text = extract_highlighted_text_from_pdf(pdf_path)
+print(highlighted_text)
